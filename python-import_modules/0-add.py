71448be1ff5451dab5ac8bfebdcae71f1ab335d2
James <8708@holbertonstudents.com>
2024-05-07 05:32:16 +0200

    Add task 0 program

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from add_0 import add  # import add function from add_0 module
+a = 1  # assign 1 to a
+b = 2  # assign 2 to b
+print("{} + {} = {}".format(a, b, add(a, b)))  # print the sum of a and b
