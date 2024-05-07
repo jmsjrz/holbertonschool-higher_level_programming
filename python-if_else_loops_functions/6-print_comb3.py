bec0d14c94cad630fe33a4f308abe43450203d71
James <8708@holbertonstudents.com>
2024-05-07 05:13:26 +0200

    Add task 6 program

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(1, 10):
+        if (i == j) or (i > j):
+            continue
+        else:
+            print("{}{}".format(i, j), end="")
+            if i + j != 17:
+                print(", ", end="")
+print("")
