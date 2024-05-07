64cbb17d9ac56f52a494716ad2e63160751f6aac
jmsjrz <panzersgrphone@gmail.com>
2024-05-08 00:36:00 +0200

    Refactor code to use formatted string for printing sum

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":  # if the script is executed, not imported
-    from add_0 import add  # import add function from add_0 module
-    a = 1  # assign 1 to a
-    b = 2  # assign 2 to b
-    print("{} + {} = {}".format(a, b, add(a, b)))  # print the sum of a and b
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
