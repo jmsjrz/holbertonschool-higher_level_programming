53ebff20fde487f8431f279b7817f766b354c7a0
James <8708@holbertonstudents.com>
2024-05-07 05:10:53 +0200

    Add task 9 program

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        number = abs(number)
+    number = number % 10
+    print("{:d}".format(number), end="")
+    return (number)
