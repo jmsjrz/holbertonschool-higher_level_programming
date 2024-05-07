4cca92f3c0e8b6908ce0eff83e86ef568cf65d60
James <8708@holbertonstudents.com>
2024-05-07 05:43:46 +0200

    Fix the program adding guard clause

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+if __name__ == "__main__":  # if the script is executed, not imported
 from add_0 import add  # import add function from add_0 module
 a = 1  # assign 1 to a
 b = 2  # assign 2 to b
