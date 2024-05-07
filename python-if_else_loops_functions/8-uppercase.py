2a9d0741f18716f894ca1fbd0172cc7a3be61f7d
James <8708@holbertonstudents.com>
2024-05-07 05:18:12 +0200

    Add task 8 program/usr/local/bin/python3

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if (ord(i) <= 122) and (ord(i) >= 97):
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end="")
+    print("")
+    
