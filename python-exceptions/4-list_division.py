265d59db0aa085ccb3e3635d637b988d92ec5054
James <8708@holbertonstudents.com>
2024-05-16 07:56:50 +0200

    Add list_division function to handle exceptions

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            #  Try to divides elements
+            result = my_list_1[i] / my_list_2[i]
+
+        except ZeroDivisionError:
+            #  Check if one element is zero
+            print("division by 0")
+            result = 0
+
+        except IndexError:
+            #  Check if list_length is out of range
+            print("out of range")
+            result = 0
+
+        except TypeError:
+            #  Check if element is been an integer or a float type
+            print("wrong type")
+            result = 0
+
+        finally:
+            #  Add the final result to the empty list 'new_list'
+            new_list.append(result)
+
+    return new_list
