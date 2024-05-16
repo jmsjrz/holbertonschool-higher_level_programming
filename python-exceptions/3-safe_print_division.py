ab0af8d92093a65380d70d3e857fb6f802f35cd4
James <8708@holbertonstudents.com>
2024-05-16 07:56:43 +0200

    Add safe_print_division function to handle ZeroDivisionError

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+
+    except ZeroDivisionError:
+        #  Check if 'b' is zero
+        result = None
+
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
