22cf4e2d25f9f87ab78ccc5aedb82fc81b2ca91a
James <8708@holbertonstudents.com>
2024-05-16 07:56:33 +0200

    Add safe_print_list_integers function to handle printing integers from a list while handling exceptions

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nb_printed = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            nb_printed += 1
+
+        except (TypeError, ValueError):
+            #  Skip in silence if Type isn't an integer or
+            #  if value is incorrect
+            pass
+
+    print()  # Print a new line
+    return nb_printed
