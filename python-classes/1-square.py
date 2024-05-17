3747bb9b67062df132901444e9e5f4d68b62a1d0
James <8708@holbertonstudents.com>
2024-05-17 14:01:11 +0200

    Add task 1 program

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+1-square.py
+
+Add the size of the class
+"""
+
+
+class Square:
+
+    def __init__(self, size):
+        self.__size = size
+
+
+"""
+Add the size of the class
+"""
