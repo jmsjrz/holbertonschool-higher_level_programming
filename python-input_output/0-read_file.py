766ad15c13a5af891c744ca450ed418e3ece881c
jmsjrz <panzersgrphone@gmail.com>
2024-05-27 11:00:58 +0200

    Fix pycodestyle for documentation too long and blank line at the end of the file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,8 +3,7 @@ def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout.
 
-    :param filename: The name of the file to read from. Defaults to an empty string.
+    :param filename: The name of the file to read. Defaults to an empty str.
     """
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end='')
-
