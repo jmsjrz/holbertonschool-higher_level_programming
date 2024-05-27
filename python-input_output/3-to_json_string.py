9215b234dc0e1ad8ab03cd1cebb3059a1be68e45
jmsjrz <panzersgrphone@gmail.com>
2024-05-27 13:58:20 +0200

    Add to_json_string function to serialize objects to JSON

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module containing the function to_json_string"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    :param my_obj: The object to be serialized to JSON.
+    :return: The JSON string representation of the object.
+    """
+    return json.dumps(my_obj)
