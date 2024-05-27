1c4fa5c37da58ed0d258a4d9dfa5c5b9a7227708
jmsjrz <panzersgrphone@gmail.com>
2024-05-27 10:57:36 +0200

    Add read_file function to read and print a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    :param filename: The name of the file to read from. Defaults to an empty string.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end='')
 
