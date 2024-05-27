8362d5eae048d73e50bb09251e4f2fb666c3f5b6
jmsjrz <panzersgrphone@gmail.com>
2024-05-27 14:03:11 +0200

    Add load_from_json_file function to read JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module containing the function load_from_json_file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    :param filename: The name of the JSON file to read from.
+    :return: The corresponding Python object.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
