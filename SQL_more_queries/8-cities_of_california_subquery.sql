9ca10ae0f215b21a03526b10714a40403159541e
jmsjrz <panzersgrphone@gmail.com>
2024-06-19 21:53:14 +0200

    Refactor SQL query to use IN clause instead of subquery

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT id, name
-FROM cities
-WHERE cities.state_id = (SELECT states.id FROM states WHERE states.name = 'California');
-ORDER BY cities.id ASC;
+SELECT id, name FROM cities
+WHERE state_id IN (SELECT id FROM states WHERE name = "California") 
+ORDER BY id ASC;
