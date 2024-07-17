8b4889e4ff193fec4e61d5da21208e2cf04c38a2
jmsjrz <panzersgrphone@gmail.com>
2024-07-17 17:53:31 +0200

    Refactor code to improve readability and maintainability

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from flask import Flask, render_template, request
 import json
 import csv
@@ -6,81 +8,62 @@ import sqlite3
 app = Flask(__name__)
 
 
-def read_json(file_path):
-    with open(file_path) as f:
-        data = json.load(f)
-    return data
+def read_json_file(filename):
+    with open(filename) as f:
+        return json.load(f)
 
 
-def read_csv(file_path):
-    data = []
-    with open(file_path) as f:
+def read_csv_file(filename):
+    products = []
+    with open(filename) as f:
         reader = csv.DictReader(f)
         for row in reader:
             row['id'] = int(row['id'])
             row['price'] = float(row['price'])
-            data.append(row)
-    return data
+            products.append(row)
+    return products
 
 
-def read_sqlite():
-    data = []
-    try:
-        conn = sqlite3.connect('products.db')
-        cursor = conn.cursor()
-        cursor.execute("SELECT id, name, category, price FROM Products")
-        rows = cursor.fetchall()
-        for row in rows:
-            data.append({
-                'id': row[0],
-                'name': row[1],
-                'category': row[2],
-                'price': row[3]
-            })
-        conn.close()
-    except sqlite3.Error as e:
-        print(f"Database error: {e}")
-        data = []
-    return data
+def read_sqlite_db():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT id, name, category, price FROM Products')
+    products = [
+        {"id": row[0], "name": row[1], "category": row[2], "price": row[3]}
+        for row in cursor.fetchall()
+    ]
+    conn.close()
+    return products
 
 
 @app.route('/products')
-def products():
+def display_products():
     source = request.args.get('source')
     product_id = request.args.get('id')
     products = []
     error = None
 
     if source == 'json':
-        try:
-            products = read_json('products.json')
-        except Exception as e:
-            error = "Error reading JSON file."
+        products = read_json_file('products.json')
     elif source == 'csv':
-        try:
-            products = read_csv('products.csv')
-        except Exception as e:
-            error = "Error reading CSV file."
+        products = read_csv_file('products.csv')
     elif source == 'sql':
-        try:
-            products = read_sqlite()
-        except Exception as e:
-            error = "Error reading database."
+        products = read_sqlite_db()
     else:
         error = "Wrong source"
 
     if product_id:
         try:
             product_id = int(product_id)
-            products = [product for product in products
-                        if product['id'] == product_id]
+            products = [product for product in products if product['id'] ==
+                        product_id]
             if not products:
                 error = "Product not found"
         except ValueError:
-            error = "Invalid product ID"
+            error = "Invalid product id"
 
-    return render_template('product_display.html',
-                           products=products, error=error)
+    return render_template('product_display.html', products=products,
+                           error=error)
 
 
 if __name__ == '__main__':
