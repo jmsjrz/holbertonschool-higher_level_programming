96fca64b0a94acbf88017c8c169c64f9a5171cfe
jmsjrz <panzersgrphone@gmail.com>
2024-07-25 18:13:14 +0200

    feat: Add JavaScript scripts for DOM manipulation

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,5 @@
+let redHeader = document.querySelector('#red_header');
+redHeader.addEventListener('click', function() {
+  let header = document.querySelector('header');
+  header.classList.add('red');
+});
